8b0a0f5195184e4b97da8313212fbca86f2374a4
Giancarmine Salucci <giancarmine@gmail.com>
2019-07-05 16:16:16 +0200

    adding VOD support

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -78,9 +78,7 @@ export default new Vuex.Store({
     },
     setVods: async function(context){
       console.log('Fetching vods list')
-      context.commit('SET_VODS', await fetch(
-        this.hostname + '/vods/'
-      ))
+      context.commit('SET_VODS', await fetch('/vods/'))
     }
 
 
