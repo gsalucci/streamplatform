8d654a54107e6ee5a6f7c3664a24b453b37f482a
Giancarmine Salucci <giancarmine@gmail.com>
2019-07-05 16:58:21 +0200

    adding VOD support

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,37 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer dark v-model="drawer" app clipped></v-navigation-drawer>
+    <v-navigation-drawer dark v-model="drawer" app clipped>
+      <v-list class="pa-1">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img src="/android-icon-192x192.png">
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>Past Streams</v-list-tile-title>
+          </v-list-tile-content>
+          <v-list-tile-action @click="refreshVods">
+            <v-icon>refresh</v-icon>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+      <v-divider light/>
+      <v-list>
+        <v-list-tile v-for="v in vods" v-bind:key="v.mtime">
+          <v-list-tile-content>
+            <v-list-tile-title>
+              {{v.name}}
+            </v-list-tile-title>
+            <v-list-tile-sub-title>
+              date: {{new Date(v.mtime)}}
+            </v-list-tile-sub-title>
+          </v-list-tile-content>
+          <v-list-tile-action @click="playVod(v)">
+            <v-icon>play</v-icon>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+
+    </v-navigation-drawer>
     <v-toolbar dark app clipped-left>
       <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>Streaming Platform</v-toolbar-title>
@@ -33,6 +64,7 @@
 <script>
 import Stream from './components/Stream/Stream'
 import Chat from './components/Chat/Chat'
+import {mapGetters} from 'vuex'
 // import NoSleep from 'nosleep.js'
 export default {
   name: 'App',
@@ -45,7 +77,17 @@ export default {
       drawer: false
     }
   },
+  computed: {
+    ...mapGetters(['vods'])
+  },
   methods: {
+    refreshVods() {
+      console.log('[App] Refreshing Vods')
+      this.$store.dispatch('setVods')
+    },
+    playVod(v) {
+      console.log('[App] playVod: '+JSON.stringify(v))
+    }
     // onClick() {
     //   console.log('[onClick] App click')
     //   this.noSleepEnabled ? console.log(this.noSleepEnabled) : this.noSleep.enable(); this.noSleepEnabled = true;
